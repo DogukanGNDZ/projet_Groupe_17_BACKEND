b2b6d7a16895c25f8cac7f8046dd3780bdbe4abe
Maxime Ferriere <maxime.ferriere@student.vinci.be>
2021-11-29 09:34:05 +0100

    création user model et route + utils auth

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../model/User.js");
+let { authorize, signAsynchronous } = require("../utils/auth");
+const jwt = require("jsonwebtoken");
+const jwtSecret = "jkjJ1235Ohno!";
+const LIFETIME_JWT = 24 * 60 * 60 * 1000 ; // 10;// in seconds // 24 * 60 * 60 * 1000 = 24h 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user list : secure the route with JWT authorization */
+router.get("/", authorize, function (req, res, next) {
+  return res.json(User.list);
 });
+
+/* POST user data for authentication */
+router.post("/login", function (req, res, next) {
+  let user = new User(req.body.username, req.body.email, req.body.password);
+  console.log("POST users/login:", User.list);
+  user.checkCredentials(req.body.email, req.body.password).then((match) => {
+    if (match) {
+      jwt.sign({ username: user.username, email: user.email }, jwtSecret,{ expiresIn: LIFETIME_JWT }, (err, token) => {
+        if (err) {
+          console.error("POST users/ :", err);
+          return res.status(500).send(err.message);
+        }
+        console.log("POST users/ token:", token);
+        //on recupère le username pour pouvoir l'afficher dans le profile
+        return res.json({ username: user.username, token });
+      });
+    } else {
+      console.log("POST users/loginRegister Error:", "Unauthentified");
+      return res.status(401).send("bad email/password");
+    }
+  })  
+});
+
+/* POST a new user */
+router.post("/", function (req, res, next) {
+  console.log("POST users/", User.list);
+  console.log("email:", req.body.email);
+  if (User.isUser(req.body.email))
+    return res.status(409).end();
+    // Ici on va géré l'utilisateur qui va être add
+  let newUser = new User(req.body.username, req.body.email, req.body.password);
+  newUser.save().then(() => {
+    console.log("afterRegisterOp:", User.list);
+    //on garde le username et l'email quand l'utilisateur se connecte
+    jwt.sign({ username: newUser.username, email:newUser.email}, jwtSecret,{ expiresIn: LIFETIME_JWT }, (err, token) => {
+      if (err) {
+        console.error("POST users/ :", err);
+        return res.status(500).send(err.message);
+      }
+      console.log("POST users/ token:", token);
+      return res.json({ username: newUser.username, token });
+    });
+  });
+});
+
+//création des routes utilisé dans gameScene
+router.get("/getMaxScore", authorize, function(req, res, next){
+  res.json({
+    score : User.getMaxScore(req.user.username)
+  });
+});
+
+router.get("/getNumberOfGame", authorize, function(req, res, next){
+  res.json({
+    score : User.getNumberOfGame(req.user.username)
+  });
+});
+
+router.get("/setMaxScore", authorize, function(req, res, next){
+  res.json({
+    score : User.getNumberOfGame(req.user.username)
+  });
+});
+
 
 module.exports = router;
